a90983a66ae53474ba92c09368112d4b0f773ef0
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-10-22 10:24:26 +0200

    first rule implemented

--- src/main/java/io/github/zbyszkop/BowlingGame.java
+++ src/main/java/io/github/zbyszkop/BowlingGame.java
@@ -1,4 +1,21 @@
 package io.github.zbyszkop;
 
 public class BowlingGame {
+    private int previousRoll;
+    private int score;
+    private boolean firstRoll = true;
+
+    public void roll(int pins) {
+        if (firstRoll) {
+            this.previousRoll = pins;
+            firstRoll = false;
+        } else {
+            this.score += this.previousRoll + pins;
+            firstRoll = true;
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- src/test/java/io/github/zbyszkop/BowlingGameTest.java
+++ src/test/java/io/github/zbyszkop/BowlingGameTest.java
@@ -1,4 +1,30 @@
 package io.github.zbyszkop;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
 public class BowlingGameTest {
+    @Test
+    public void shouldScoreAsingleFrame() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(3);
+        assertThat(bowlingGame.getScore()).isEqualTo(0);
+        bowlingGame.roll(5);
+        assertThat(bowlingGame.getScore()).isEqualTo(8);
+    }
+
+    @Test
+    public void shouldScoreATwoFrames() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(3); // first roll a 1st frame
+        assertThat(bowlingGame.getScore()).isEqualTo(0);
+        bowlingGame.roll(5); // end of a 1st frame
+        assertThat(bowlingGame.getScore()).isEqualTo(8);
+        bowlingGame.roll(4); // first roll a 2nd frame
+        assertThat(bowlingGame.getScore()).isEqualTo(8);
+        bowlingGame.roll(2); // first roll a 2nd frame
+        assertThat(bowlingGame.getScore()).isEqualTo(14);
+    }
 }
