f372ac369844f636c1b63b6274ba2feb78a3b840
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-11-05 17:48:38 +0100

    first rule implemented

--- src/main/java/io/github/zbyszkop/BowlingGame.java
+++ src/main/java/io/github/zbyszkop/BowlingGame.java
@@ -1,5 +1,19 @@
 package io.github.zbyszkop;
 
-public class BowlingGame
-{
+public class BowlingGame {
+
+    private int firstRoll = -1;
+    private int score = 0;
+    public void roll(int pins) {
+        if (firstRoll == -1) {
+            firstRoll = pins;
+        } else {
+            score += firstRoll + pins;
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
 }
--- src/test/java/io/github/zbyszkop/BowlingGameTest.java
+++ src/test/java/io/github/zbyszkop/BowlingGameTest.java
@@ -1,5 +1,30 @@
 package io.github.zbyszkop;
 
-public class BowlingGameTest
-{
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    public void shouldTallyTScoreOnlyAfterTheFrame() {
+        BowlingGame game = new BowlingGame();
+        game.roll(3);
+        assertThat(game.getScore()).isEqualTo(0);
+        game.roll(5);
+        assertThat(game.getScore()).isEqualTo(8);
+    }
+
+    @Test
+    public void shouldCountTheWholeGame() {
+        BowlingGame game = new BowlingGame();
+
+        for (int i =0; i < 10; i++) {
+            game.roll(3);
+            game.roll(5);
+        }
+        assertThat(game.getScore()).isEqualTo(80);
+    }
 }
