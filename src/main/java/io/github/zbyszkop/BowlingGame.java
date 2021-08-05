1296b7957f40f4d7eb96218a2897706a85a2ca8f
tanny411 <aysha.kamal7@gmail.com>
2021-08-05 16:19:41 +0600

    TDD - passes until one strike

--- src/main/java/io/github/zbyszkop/BowlingGame.java
+++ src/main/java/io/github/zbyszkop/BowlingGame.java
@@ -3,10 +3,38 @@ package io.github.zbyszkop;
 public class BowlingGame {
     int rollCount = 0;
     int lastPins = 0;
+    boolean strikeProgress = false;
     int score = 0;
+    boolean spare = false;
+    boolean strike = false;
+
     public void roll(int pins) {
+        if(strike){
+            if(!strikeProgress){
+                strikeProgress = true;
+            }
+            else{
+                score += 10+lastPins+pins;
+                strike = false;
+            }
+        }
         if (isEndofFrame()) {
-            score += pins + lastPins;
+            if(pins+lastPins != 10){
+                score += pins + lastPins ;
+            }
+            else{
+                spare = true;
+            }
+        }
+        else{
+            if(pins==10){
+                rollCount++;
+                strike = true;
+            }
+            if(spare){
+                score += 10+pins;
+                spare = false;
+            }
         }
         lastPins = pins;
         rollCount++;
--- src/test/java/io/github/zbyszkop/BowlingGameTest.java
+++ src/test/java/io/github/zbyszkop/BowlingGameTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class BowlingGameTest {
 
     @Test
-    public void shouldAllowRollingAndGettingScore() {
+    public void shouldCorrectlyScoreTwoFrames() {
         BowlingGame bowlingGame = new BowlingGame();
         bowlingGame.roll(4);  //first frame
         assertEquals(0, bowlingGame.getScore());
@@ -17,5 +17,43 @@ public class BowlingGameTest {
         assertEquals(9, bowlingGame.getScore());
         bowlingGame.roll(2);
         assertEquals(14, bowlingGame.getScore());
+    }
+
+    @Test
+    public void shouldCorrectlyTallySpare(){
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(2);// 1st
+        bowlingGame.roll(8);
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(4); //2nd
+        assertEquals(14, bowlingGame.getScore());
+        bowlingGame.roll(3);
+        assertEquals(21, bowlingGame.getScore());
+        bowlingGame.roll(5); //3rd
+        assertEquals(21, bowlingGame.getScore());
+    }
+
+    @Test
+    public void shouldCorrectlyTallyStrike(){
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(10);// 1st
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(2); //2nd
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(3);
+        assertEquals(20, bowlingGame.getScore());
+    }
+
+    @Test
+    public void shouldCorrectlyTallyTwoStrikes(){
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(10);// 1st
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(10); //2nd
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(2); //3rd
+        assertEquals(22, bowlingGame.getScore());
+        bowlingGame.roll(3);
+        assertEquals(42, bowlingGame.getScore());
     }
 }
