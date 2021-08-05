84b929e6451491a3757deb79f35ef0d1a0285dd1
Zbyszko Papierski <zpapierski@wikimedia.org>
2021-08-05 10:50:53 +0200

    TDD - until rule first

--- src/main/java/io/github/zbyszkop/BowlingGame.java
+++ src/main/java/io/github/zbyszkop/BowlingGame.java
@@ -1,4 +1,22 @@
 package io.github.zbyszkop;
 
 public class BowlingGame {
+    int rollCount = 0;
+    int lastPins = 0;
+    int score = 0;
+    public void roll(int pins) {
+        if (isEndofFrame()) {
+            score += pins + lastPins;
+        }
+        lastPins = pins;
+        rollCount++;
+    }
+
+    private boolean isEndofFrame() {
+        return rollCount % 2 == 1;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
--- src/test/java/io/github/zbyszkop/BowlingGameTest.java
+++ src/test/java/io/github/zbyszkop/BowlingGameTest.java
@@ -1,4 +1,21 @@
 package io.github.zbyszkop;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class BowlingGameTest {
+
+    @Test
+    public void shouldAllowRollingAndGettingScore() {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(4);  //first frame
+        assertEquals(0, bowlingGame.getScore());
+        bowlingGame.roll(5);
+        assertEquals(9, bowlingGame.getScore());
+        bowlingGame.roll(3);  //second frame
+        assertEquals(9, bowlingGame.getScore());
+        bowlingGame.roll(2);
+        assertEquals(14, bowlingGame.getScore());
+    }
 }
